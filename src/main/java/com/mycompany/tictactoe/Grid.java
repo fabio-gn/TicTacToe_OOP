4a70e6078a116bd6fafac43ab9a10a3722313d12
fabio giannasi <fabio.giannasi@outlook.com>
2026-04-26 01:01:09 +0200

    sviluppato classe grid

--- src/main/java/com/mycompany/tictactoe/Grid.java
+++ src/main/java/com/mycompany/tictactoe/Grid.java
@@ -4,10 +4,30 @@
  */
 package com.mycompany.tictactoe;
 
+import com.mycompany.tictactoe.exceptions.InvalidGridSizeException;
+
 /**
  *
  * @author Fabio
  */
 public class Grid {
+    private static int[] gridSize = {3,3};
+    private char[][] grid;
+    
+    public Grid(){
+        
+    }
+
+    public char[][] getGrid() {
+        return grid;
+    }
+
+    protected void setGrid(int[] size) throws InvalidGridSizeException{
+        if(size.length != 2){
+           throw new InvalidGridSizeException();
+        }
+        this.grid = grid;
+    }
+    
     
 }
